378964efeaf7a6874f06fadbd40c38acdddfa965
Brianna Gaines <briannagaines31@gmail.com>
2022-04-13 02:07:14 -0600

    gives up on trying to get sum to work

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = 3001
 const lineItem = require("./models/budget.js");
 const morgan = require("morgan")
 const methodOverride = require("method-override")
+let bankAcct = [];
+
 
 app.use(express.urlencoded({extended: false}));
 app.use(morgan("tiny"))
@@ -15,6 +17,7 @@ app.use("/static", express.static("public"))
 app.get("/budget/", (req,res)=>{
     res.render("index.ejs", {allLineItems: lineItem})
 })
+
   //New
 app.get("/budget/new", (req,res)=>{
     res.render("new.ejs")
@@ -24,7 +27,6 @@ app.get("/budget/new", (req,res)=>{
  //Create
  app.post('/budget', (req, res) => {
     lineItem.push(req.body);
-    console.log(lineItem);
     res.redirect('/budget');
   });
 
@@ -33,7 +35,20 @@ app.get("/budget/new", (req,res)=>{
     res.render("show.ejs", { item: lineItem[req.params.id] });
   });
 
+  //adds sum but couldnt figure out how to get it to add new amounts/connect to my index.ejs file
+  
+//   for (amount in lineItem){
+//     currentBalance = (lineItem[amount].amount)
+//     bankAcct.push(currentBalance)
+//  }
+// let sum = 0
+//  for (let i = 0; i < bankAcct.length; i++) {
+//     sum += bankAcct[i];
+// }
+// console.log(sum)
 
 app.listen(PORT, () =>{
     console.log(`listening on port ${PORT}`)
 })
+
+
