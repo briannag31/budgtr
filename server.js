e9e429dd0296a877a0691c8a4ff237ef0c9b2552
Brianna Gaines <briannagaines31@gmail.com>
2022-04-13 20:30:14 -0600

    final

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const app = express()
 const PORT = 3001
 const lineItem = require("./models/budget.js");
 const morgan = require("morgan")
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
 let bankAcct = [];
+let currentBalance=0 
+    for(let amount in lineItem) {
+       currentBalance=(lineItem[amount].amount)
+       bankAcct.push(currentBalance)
+    }  
+    let sum = 0
+  for (let i = 0; i < bankAcct.length; i++) {
+  sum += bankAcct[i];
+}
+
+console.log(sum)
+
+
 
 
 app.use(express.urlencoded({extended: false}));
@@ -13,39 +26,29 @@ app.use(methodOverride("_method"))
 app.use("/static", express.static("public"))
 
 
+
 //Index
 app.get("/budget/", (req,res)=>{
     res.render("index.ejs", {allLineItems: lineItem})
 })
-
   //New
 app.get("/budget/new", (req,res)=>{
-    res.render("new.ejs")
- })
+    res.render("new.ejs", {items: lineItem})
  
+ })
+
 
  //Create
  app.post('/budget', (req, res) => {
     lineItem.push(req.body);
     res.redirect('/budget');
   });
+  
 
  //Show
  app.get("/budget/:id", (req, res) => {
     res.render("show.ejs", { item: lineItem[req.params.id] });
   });
-
-  //adds sum but couldnt figure out how to get it to add new amounts/connect to my index.ejs file
-  
-//   for (amount in lineItem){
-//     currentBalance = (lineItem[amount].amount)
-//     bankAcct.push(currentBalance)
-//  }
-// let sum = 0
-//  for (let i = 0; i < bankAcct.length; i++) {
-//     sum += bankAcct[i];
-// }
-// console.log(sum)
 
 app.listen(PORT, () =>{
     console.log(`listening on port ${PORT}`)
